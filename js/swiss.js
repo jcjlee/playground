0114fd78efa7c369550d44cae7b8207765b158b8
jason lee <leejcj@gmail.com>
2013-12-14 19:05:02 -0800

    Finished math on random pairings

--- js/swiss.js
+++ js/swiss.js
@@ -10,6 +10,7 @@ function SwissCtrl($scope) {
 	$scope.totalRounds = 0;
 	$scope.roundsPlayed = 0;
 	$scope.currentRound = 0;
+	$scope.pairings = [];
 
 	$scope.getPlayerTotal = function() {
 		return $scope.players.length;
@@ -20,8 +21,8 @@ function SwissCtrl($scope) {
 	}
 	$scope.generatePairings = function() {
 		$scope.round=1;
-		$scope.playerRecords = numberPlayers($scope.players);
-		displayStandings();
+		$scope.players = assignPlayerNumbers($scope.players)
+		$scope.pairings = initialPairings($scope.players);
 	}
 }
 /** Returns a list of players with each player numbered.
@@ -34,17 +35,85 @@ function assignPlayerNumbers (list) {
 	return list;
 }
 
+function isEven (number) {
+	return !isNaN(number) && (number % 2 == 0);
+}
+
+function createMatchObj (p1, p2, round) {
+	var match = {};
+	if (p1 == '0') {
+		match.player1 = "BYE";
+		match.player2 = p2;
+	}
+	else if (p2 == '0') {
+		match.player1 = p1;
+		match.player2 = "BYE";
+	}
+	else {
+		match.player1 = p1;
+		match.player2 = p2;
+		match.round = round;
+	}
+	return match;
+
+}
+
+function initialPairings (playerList) {
+	var pairings = [],
+		FIRST_ROUND = 1,
+		BYE_NUMBER = 0,
+		list = [],
+		expectedMatches;
+	for (var i = 0; i < playerList.length; i++) {
+		list.push(playerList[i]);
+	}
+
+	if (!isEven((list.length))) {
+		var suckaIndex = getRandomIndex(list.length),
+			sucka = list[suckaIndex];
+		pairings.push(createMatchObj(sucka, BYE_NUMBER, 1));
+		if (suckaIndex > -1) {
+			list.splice(suckaIndex, 1);
+		}
+	}
+	expectedMatches = list.length/2;
+	for (var i = 0; i < expectedMatches; i++) {
+		var p1Index = getRandomIndex(list.length),
+			player1 = list[p1Index];
+		if (p1Index > -1) {
+			console.log(p1Index + " " + list.length);
+			list.splice(p1Index, 1);
+		}
+		var p2Index = getRandomIndex(list.length),
+			player2 = list[p2Index];
+		if (p2Index > -1) {
+			console.log(p2Index + " " + list.length);
+			list.splice(p2Index, 1);
+		}
+		pairings.push(createMatchObj(player1, player2, FIRST_ROUND));
+	}
+	return pairings;
+}
+
+function getRandomIndex (length) {
+	var random = Math.floor((Math.random()*length));
+	return random;
+}
+
+
 /** Gets the standings for the current state 
  *
  */
 function getStandings (currentRound, playerList, matchList) {
-	var standings = [];
+	var standings = [],
+		rankings = {};
 	if (currentRound > 0) {
 		for (var i = 0; i < matchList.length; i++) {
-			//Get players in order of points
-			//Get players OMW%
-			//Get players GW%
-			//Get players OGW%
+			// Get players in order of points
+
+			// Get players OMW%
+			// Get players GW%
+			// Get players OGW%
 		}
 	}
 	return standings;
